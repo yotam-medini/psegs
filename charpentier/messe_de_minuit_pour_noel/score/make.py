f084c50f65c25e59c506c817bf67cd1bd3d7d03d
Yotam Medini <yotam.medini@gmail.com>
2024-11-01 21:36:24 +0200

    charpentier ready to print

--- charpentier/messe_de_minuit_pour_noel/score/make.py
+++ charpentier/messe_de_minuit_pour_noel/score/make.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import glob
 import os
 import subprocess
 import sys
@@ -28,54 +29,75 @@ def convert_page(pn: int):
     else:
         sample_index = SAMPLE_TARGET_TO_INDEX.get(pn)
         if sample_index is None:
-            if pn in [54, 56]:
+            if pn in [42, 54, 56]:
                 cmd = f"cpdf {ALTERNATIVE} {pn - 25} -o p/t0.pdf"
             else:
                 cmd = f"cpdf {MAIN} {pn - 1} -o p/t0.pdf"
             syscmd(cmd)
             cmd = "cpdf -scale-to-fit a4portrait p/t0.pdf -o p/t1.pdf"
             syscmd(cmd)
-            rect = "20 842"
+
+            lrectx = {
+                39: 40, 43: 34,
+                # 8: 60, 9: 40, 10: 40, 12: 40, 16: 40, 22: 40,
+            }.get(pn, 20)
+            lrect = f"{lrectx} 842"
             cmd0 = (f"cpdf p/t1.pdf -add-rectangle zzz -pos-left zzz"
-                f" -color red -o p/t2.pdf")
+                f" -color white -o p/t2.pdf")
             cmd = cmd0.split()
-            cmd[3] = rect
+            cmd[3] = lrect
             cmd[5] = "0 0"
             subprocess.run(cmd)
+
+            rrectx = {
+                8: 40, 9:40, 39: 40, 56: 0,
+            }.get(pn, 20)
+            rrect = f"{rrectx} 842"
             cmd0 = (f"cpdf p/t2.pdf -add-rectangle zzz -pos-left zzz"
-                f" -color blue -o p/t3.pdf")
+                f" -color white -o p/t3.pdf")
             cmd = cmd0.split()
-            cmd[3] = rect
-            cmd[5] = "585 0"
+            cmd[3] = rrect
+            # cmd[5] = "595 0"
+            cmd[5] = f"{595 - rrectx} 0"
             vlog(' '.join(cmd))
             subprocess.run(cmd)
-            x_shift = "-16mm" if pn % 2 == 0 else "16mm"
-            cmd = f'cpdf -shift "{x_shift} 0mm" -o {target_pdf} p/t3.pdf'
+            x_shift = "-8mm" if pn % 2 == 0 else "8mm" # default
+            x_shift = {
+                54: "-12mm", 6: "-16mm", 56: "-12mm",
+            }.get(pn, x_shift)
+            cmd = f'cpdf -shift "{x_shift} 0mm" p/t3.pdf -o {target_pdf}'
             syscmd(cmd)
         else:
-            cmd = f"cpdf -o p/t.pdf {SAMPLE} {sample_index}"
+            cmd = f"cpdf {SAMPLE} {sample_index} -o p/t0.pdf"
             syscmd(cmd)
             x_shift = "-8mm" if pn % 2 == 0 else "8mm"
-            cmd = f'cpdf -shift "{x_shift} 0mm" -o {target_pdf} p/t.pdf'
+            cmd = f'cpdf -shift "{x_shift} 0mm" p/t0.pdf -o p/p1.pdf'
+            syscmd(cmd)
+            cmd = f'cpdf -scale-to-fit a4portrait p/p1.pdf -o {target_pdf}'
             syscmd(cmd)
 
 def convert_pages():
     os.makedirs("p", exist_ok=True)
     # for pn in range(3, 58):
     if not os.path.exists("p/p01.pdf"):
-        cmd = f"cpdf {MAIN} 1 -o p/p01.pdf"
+        cmd = f"cpdf {MAIN} 1 -o p/t0.pdf"
+        syscmd(cmd)
+        cmd = f"cpdf -scale-to-fit a4portrait p/t0.pdf 1 -o p/p01.pdf"
         syscmd(cmd)
     if not os.path.exists("p/p02.pdf"):
         # cmd = "cpdf -o p/p02.pdf -blank -paper a4"
-        cmd = "cpdf -create-pdf a4portrait -o p/p02..pdf"
+        cmd = "cpdf -create-pdf a4portrait -o p/p02.pdf"
         syscmd(cmd)
     for pn in range(3, 58):
         convert_page(pn)
         
 def make_best():
     convert_pages()
-    cmd = f"cpdf -o {BEST} p/p??.pdf"
-    syscmd(cmd)
+    if not os.path.exists(BEST):
+        pages = glob.glob("p/p??.pdf")
+        pages.sort()
+        cmd = f"cpdf {' '.join(pages)} -o {BEST}"
+        syscmd(cmd)
     
 if __name__ == "__main__":
     rc = 0
